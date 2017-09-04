8ea77d6e969075dc6906dcfff46972d49a46a35d
Tom Mason <wheybags@wheybags.com>
2017-09-04 21:11:53 +0200

    simple_exec: handle nonexistant command

--- simple_exec.c
+++ simple_exec.c
@@ -1,4 +1,7 @@
-// test with: gcc simple_exec.c -DTEST_EXEC_MAIN -g && valgrind --track-fds=yes ./a.out
+// Test with: gcc simple_exec.c -DTEST_EXEC_MAIN -g && valgrind --track-fds=yes ./a.out
+// Will show one child process with leaks - that's fine, it's what happens if you try to run an executable
+// that doesn't exist - it returns an error on a special error pipe and immediately exits.
+
 // adapted from: https://stackoverflow.com/a/479103
 
 #include <stdio.h>
@@ -15,7 +18,13 @@ enum PIPE_FILE_DESCRIPTORS
   WRITE_FD = 1
 };
 
-int runCommandArray(char** stdOut, int* stdOutByteCount, char* const* allArgs)
+enum RUN_COMMAND_ERROR
+{
+    COMMAND_RAN_OK = 0,
+    COMMAND_NOT_FOUND = 1
+};
+
+int runCommandArray(char** stdOut, int* stdOutByteCount, int* returnCode, char* const* allArgs)
 {
     int bufferSize = 256;
     char buffer[bufferSize + 1];
@@ -31,6 +40,9 @@ int runCommandArray(char** stdOut, int* stdOutByteCount, char* const* allArgs)
 
     int childToParent[2];
     assert(pipe(childToParent) == 0);
+
+    int errPipe[2];
+    assert(pipe(errPipe) == 0);
 
     pid_t pid;
     switch( pid = fork() )
@@ -49,14 +61,19 @@ int runCommandArray(char** stdOut, int* stdOutByteCount, char* const* allArgs)
             // unused
             assert(close(parentToChild[WRITE_FD]) == 0);
             assert(close(childToParent[READ_FD ]) == 0);
+            assert(close(errPipe[READ_FD]) == 0);
             
             const char* command = allArgs[0];
             execvp(command, allArgs);
 
-            // We should finish closing parentToChild and childToParent here, 
-            // but execvp will never return anyway.
+            char err = 1;
+            write(errPipe[WRITE_FD], &err, 1);
+            
+            close(errPipe[WRITE_FD]);
+            close(parentToChild[READ_FD]);
+            close(childToParent[WRITE_FD]);
 
-            assert(0 && "This line should never be reached!!!");
+            exit(0);
         }
 
 
@@ -65,6 +82,7 @@ int runCommandArray(char** stdOut, int* stdOutByteCount, char* const* allArgs)
             // unused
             assert(close(parentToChild[READ_FD]) == 0);
             assert(close(childToParent[WRITE_FD]) == 0);
+            assert(close(errPipe[WRITE_FD]) == 0);
 
             while(1)
             {
@@ -75,20 +93,36 @@ int runCommandArray(char** stdOut, int* stdOutByteCount, char* const* allArgs)
                     {
                         int status = 0;
                         assert(waitpid(pid, &status, 0) == pid);
-                        
+
                         // done with these now
                         assert(close(parentToChild[WRITE_FD]) == 0);
                         assert(close(childToParent[READ_FD]) == 0);
-                       
+
+                        char errChar = 0;
+                        read(errPipe[READ_FD], &errChar, 1);
+                        close(errPipe[READ_FD]);
+
+                        if(errChar)
+                        {
+                            free(dataReadFromChild); 
+                            return COMMAND_NOT_FOUND;
+                        }
+                        
                         // free any un-needed memory with realloc + add a null terminator for convenience
                         dataReadFromChild = (char*)realloc(dataReadFromChild, dataReadFromChildUsed + 1);
                         dataReadFromChild[dataReadFromChildUsed] = '\0';
                         
                         if(stdOut != NULL)
                             *stdOut = dataReadFromChild;
+                        else
+                            free(dataReadFromChild);
+
                         if(stdOutByteCount != NULL)
                             *stdOutByteCount = dataReadFromChildUsed;
-                        return WEXITSTATUS(status);
+                        if(returnCode != NULL)
+                            *returnCode = WEXITSTATUS(status);
+
+                        return COMMAND_RAN_OK;
                     }
                     case -1:
                     {
@@ -113,7 +147,7 @@ int runCommandArray(char** stdOut, int* stdOutByteCount, char* const* allArgs)
     }
 }
 
-int runCommand(char** stdOut, int* stdOutByteCount, char* command, ...)
+int runCommand(char** stdOut, int* stdOutByteCount, int* returnCode, char* command, ...)
 {
     va_list vl;
     va_start(vl, command);
@@ -143,7 +177,7 @@ int runCommand(char** stdOut, int* stdOutByteCount, char* command, ...)
 
     va_end(vl);
 
-    int retval = runCommandArray(stdOut, stdOutByteCount, allArgs);
+    int retval = runCommandArray(stdOut, stdOutByteCount, returnCode, allArgs);
     free(allArgs);
     return retval;
 }
@@ -154,12 +188,18 @@ int main(int argc, char** argv)
     // get stdout, and return code
     char* stdOut = NULL;
     int byteCount = 0;
-    int exitCode = runCommand(&stdOut, &byteCount, "ls", "-l", (char*)NULL);
+    int exitCode = 0;
+    int err = runCommand(&stdOut, &byteCount, &exitCode, "ls", "-l", (char*)NULL);
+    assert(err == COMMAND_RAN_OK); // doesn't mean the command succeeded, just that invoking it didn't fail
     printf("EXIT CODE: %d, STDOUT SIZE: %d bytes, STDOUT: %s", exitCode, byteCount, stdOut);
     free(stdOut);
 
     // or don't
-    runCommand(NULL, NULL, "touch", "blah", (char*)NULL); 
+    runCommand(NULL, NULL, NULL, "touch", "blah", (char*)NULL); 
+    
+    // This will show leaks in valgrind - see comment at top of file
+    err = runCommand(NULL, NULL, NULL, "thisCommandDoesntExist", (char*)NULL);
+    assert(err == COMMAND_NOT_FOUND);
 
     return 0;
 }
